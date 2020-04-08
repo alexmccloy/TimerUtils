40d0c20b7e2d1372e8dd23f9a89359cafdac524e
alexmccloy <amccloy@gmail.com>
2020-04-08 20:17:40 +1000

    Added some utility methods

---
+++ Amccloy.TimerUtils/TimerUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Amccloy.TimerUtils
+{
+    public static class TimerUtils
+    {
+        public static async Task DoAfter(Action action, int milliseconds)
+        {
+            await Task.Delay(milliseconds);
+            action.Invoke();
+        }
+        
+        //TODO add DoEvery
+    }
+}
