dd811f489cdc163881c14465f8221b8611f5bbd2
alexmccloy <amccloy@gmail.com>
2020-04-08 21:15:16 +1000

    Add cancellation token support to DoAfter

--- Amccloy.TimerUtils/TimerUtils.cs
+++ Amccloy.TimerUtils/TimerUtils.cs
@@ -1,14 +1,18 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Amccloy.TimerUtils
 {
     public static class TimerUtils
     {
-        public static async Task DoAfter(Action action, int milliseconds)
+        public static async Task DoAfter(Action action, int milliseconds, CancellationToken cancellationToken)
         {
-            await Task.Delay(milliseconds);
-            action.Invoke();
+            await Task.Delay(milliseconds, cancellationToken);
+            
+            if (!cancellationToken.IsCancellationRequested) {
+                action.Invoke();
+            }
         }
         
         //TODO add DoEvery
